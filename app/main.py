09785f968001d5b96db4d835fb748c643024ac9f
Priyanshu Dwivedi <pdxdwivedi2005@gmail.com>
2025-11-24 21:23:02 +0530

    Added put and delete endpoint

--- app/main.py
+++ app/main.py
@@ -26,3 +26,20 @@ def add_entry(entry:LedgerEntry):
 @app.get("/read")
 def get_all_entries():
     return ledger
+
+@app.put("/update/{entry_id}")
+def update_entry(entry_id:int,updated_entry:LedgerEntry):
+    for index, entry in enumerate(ledger):
+        if entry.id==entry_id:
+            ledger[index]=updated_entry
+            return{"message":"entry updated successfully"}
+    return{"error":"entry not found"}
+
+
+@app.delete("/delete/{entry_id}")
+def delete_entry(entry_id:int):
+    for index,entry in enumerate(ledger):
+        if entry.id==entry_id:
+            ledger.pop(index)
+            return{"message":"entry deleted successfully"}
+        return{"error":"entry not found"}
