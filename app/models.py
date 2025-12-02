ef6cd7996cc3e99576e1054f2969c94a6bed71cd
Priyanshu Dwivedi <pdxdwivedi2005@gmail.com>
2025-12-02 16:05:35 +0530

    fixed the error which was causing sqlalchemy to not create the table

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Float, Date
 from app.database import Base
 
 class Transactions(Base):
-    _tablename_ = "ledger_entries"
+    __tablename__ = "ledger_entries"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
