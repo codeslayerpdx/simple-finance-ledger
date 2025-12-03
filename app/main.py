015a31413071916ae4c91f45f21113228819cee7
Priyanshu Dwivedi <pdxdwivedi2005@gmail.com>
2025-12-03 17:48:41 +0530

    Add Items and Cart tables with endpoints, update schemas and main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from app.models import Transactions
 from app.database import get_db, Base, engine
 from app import models
 from typing import List
+from app.models import Item, Cart   
+from app.schemas import ItemCreate, ItemOut, CartCreate, CartOut 
 
 
 app=FastAPI()
@@ -62,3 +64,64 @@ def delete_entry(entry_id: int, db: Session = Depends(get_db)):
     db.delete(txn)
     db.commit()
     return {"message": "Entry deleted successfully"}
+
+
+
+
+
+@app.post("/items/", response_model=ItemOut)
+def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = Item(
+        item_name=item.item_name,
+        price=item.price,
+        description=item.description,
+        expiry_date=item.expiry_date
+    )
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+
+
+@app.get("/items/", response_model=list[ItemOut])
+def get_items(db: Session = Depends(get_db)):
+    return db.query(Item).all()
+
+
+
+
+@app.post("/cart/", response_model=CartOut)
+def create_cart(cart: CartCreate, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.item_id == cart.item_id).first()
+    if not item:
+        return {"error": "Item not found"}
+
+    total_price = item.price  
+
+    db_cart = Cart(
+        item_id=cart.item_id,
+        cart_created=cart.cart_created,
+        total_price=total_price
+    )
+
+    db.add(db_cart)
+    db.commit()
+    db.refresh(db_cart)
+    return db_cart
+
+
+
+
+@app.get("/cart/{cart_id}", response_model=CartOut)
+def get_cart(cart_id: int, db: Session = Depends(get_db)):
+    return db.query(Cart).filter(Cart.cart_id == cart_id).first()
+
+
+
+
+
+@app.get("/cart/", response_model=list[CartOut])
+def get_all_carts(db: Session = Depends(get_db)):
+    return db.query(Cart).all()
