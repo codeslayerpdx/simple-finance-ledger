24a4b184fc834efee4102e3923cb2596b22611a2
Priyanshu Dwivedi <pdxdwivedi2005@gmail.com>
2025-12-03 10:07:15 +0530

    Fix table name: change tablename from ledger_entries to transactions

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Float, Date
 from app.database import Base
 
 class Transactions(Base):
-    __tablename__ = "ledger_entries"
+    __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
